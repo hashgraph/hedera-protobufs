a02075c20179249637c9c11d4d8df2a046801de5
tinker-michaelj <michael.tinker@hedera.com>
2022-08-11 20:52:42 -0500

    Support multiple output formats
    Signed-off-by: tinker-michaelj <michael.tinker@hedera.com>

--- run/gen-proto-docs.sh
+++ run/gen-proto-docs.sh
@@ -1,5 +1,7 @@
 #! /bin/sh
 DIR=${1:-'services'}
+FORMAT=${2:-'html'}
+OUTPUT=${3:-'index.html'}
 case $DIR in
   services)
     cd $DIR
@@ -12,5 +14,5 @@ esac
 docker run --rm \
   -v $(pwd):/out \
   -v $(pwd):/protos \
-  pseudomuto/protoc-gen-doc
-mv index.html ../
+  pseudomuto/protoc-gen-doc:latest --doc_opt=${FORMAT},${OUTPUT}
+mv $OUTPUT ../
